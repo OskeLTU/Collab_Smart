84c98615aab1a1219c67c06aed73cf8472a6fdfe
Oske <oskepower@gmail.com>
2025-09-15 11:06:13 +0200

    Update Variable.h

---
+++ frame for car/src/Variable.h
@@ -0,0 +1,9 @@
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+int M1F = ;   // Motor 1 forward
+int M1B = ;   // Motor 1 backward
+int M2F = ;   // Motor 2 forward
+int M2B = ;   // Motor 2 backward
+
+#endif
